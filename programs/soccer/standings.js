4606a1e7975a19acba32abd9d958e711251df63c
Osama <osama.hafez@mail.utoronto.ca>
2021-07-04 17:50:17 -0400

    fix fetch on document load

--- programs/soccer/standings.js
+++ programs/soccer/standings.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
+    getStandings();
     getSchedule();
 });
 
 function getStandings() {
-
-    let standings = $("#standings");
-
     $.ajax({
         url: "http://offlinequran.com:3001/api/Men's%20League/getTeams",
         //url: "https://muslimathleticassociation.org:3001/api/Men's%20League/getTeams",
@@ -37,19 +35,16 @@ function getStandings() {
 }
 
 function getSchedule() {
-
-    let standings = $("#schedule");
-
     $.ajax({
-        url: "http://offlinequran.com:3001/api/Men's%20League/getTeams",
-        //url: "https://muslimathleticassociation.org:3001/api/Men's%20League/getTeams",
+        url: "http://offlinequran.com:3001/api/fixtures/1",
+        //url: "https://muslimathleticassociation.org:3001/api/fixtures/1",
         method: "GET"
     })
         .done((res) => {
             console.log(res);
             console.log(res.data);
             for (var fixture = 0; fixture < res.data.length; fixture++) {
-                $("#standings tbody").append("<tr>" +
+                $("#schedule tbody").append("<tr>" +
                 "<td>" + res.data[fixture].team1 +
                 "<td>" + res.data[fixture].score1 +
                 "<td>" + res.data[fixture].score2 +
